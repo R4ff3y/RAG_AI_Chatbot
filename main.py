2665ff59f406d75d03685f273f92f34c5052b626
R4ff3y <tobias.wagenpfeil@gmx.de>
2024-06-04 11:38:42 +0200

    Changed main.py, added UI

--- main.py
+++ main.py
@@ -1,5 +1,12 @@
-from langchain_intro.chatbot import review_chain
+from RAG_Model.chatbot import review_chain
+import gradio as gr
 
 # question = str(input("Please enter your question: "))
 
-print(review_chain.invoke(question))
+def ask_chatbot(question, history):
+    return review_chain.invoke(question)
+
+demo = gr.ChatInterface(fn=ask_chatbot, title="Hydac GPT", 
+                        theme='soft')
+
+demo.launch()
